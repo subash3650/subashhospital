28223279dc815ddb0fdcda00b94069adedc99d46
Subash P <131967537+subash3650@users.noreply.github.com>
2024-03-12 20:57:27 +0530

    Add files via upload

---
+++ src/Scripts/reviews.js
@@ -0,0 +1,52 @@
+export const customerReviews = [
+    {
+      "name": "Jagadheesan G",
+      "location": "Chennai,India",
+      "message": "Health Plus transformed my healthcare experience! The online consultations were so convenient, and the doctors were knowledgeable and caring."
+    },
+    {
+      "name": "Magi Balan M",
+      "location": "Tenkasi,India",
+      "message": "I found the perfect specialist for my condition through Health Plus. The personalized treatment plan made all the difference. Thank you for prioritizing my health!"
+    },
+    {
+      "name": "Nantha",
+      "location": "Trichy, India",
+      "message": "Booking appointments was a breeze, and the service exceeded my expectations. The doctors truly listen and provide effective solutions. 5 stars!"
+    },
+    {
+      "name": "Deepak",
+      "location": "Florida, India",
+      "message": "Health Plus is a game-changer! The emergency care saved me during a critical situation. Grateful for their quick and efficient support."
+    },
+    {
+      "name": "Karthik Yogesh",
+      "location": "Chennai,India",
+      "message": "I used to dread dental visits, but Health Plus made it a pleasant experience. The dentist was gentle and professional. I'll definitely be back!"
+    },
+    {
+      "name": "Vishnu",
+      "location": "Virudhachalam,India",
+      "message": "Finally, a healthcare platform that puts patients first! The heart disease service provided comprehensive care and regular follow-ups. Thank you, Health Plus!"
+    },
+    {
+      "name": "Aadhitya S",
+      "location": "Trichy, India",
+      "message": "I've been using Health Plus for a variety of health concerns, and each time, I received exceptional care. It's my go-to for all medical needs!"
+    },
+    {
+      "name": "Subash P",
+      "location": "Cuddalore,India",
+      "message": "I highly recommend Health Plus for online consultations. It's convenient, secure, and the doctors are top-notch. Great job, team!"
+    },
+    {
+      "name": "HariHaran K",
+      "location": "Cuddalore,India",
+      "message": "The convenience of accessing medical notes online was fantastic. Health Plus made managing my health records hassle-free! Best Healthcare services."
+    },
+    {
+      "name": "Vigneshwar P",
+      "location": "Coimbatore,India",
+      "message": "As a busy professional, Health Plus has been a lifesaver. Quick prescriptions and refills without compromising on quality care. Thank you!"
+    }
+];
